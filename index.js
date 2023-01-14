b0f75ee5cc82eab81c2e846fdc314430dc00616c
Njaaga-Gakure <njaagagakure@gmail.com>
2023-01-14 16:07:43 +0300

    Create function and render their return value on the screen

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-ReactDOM.render(<p>This is my first React code</p>, document.getElementById("root"));
+function Maincontent(){
+    return (<h1>We did it!!</h1>);
+}
+function Decl(){
+    return (<h1 className="header">This is the declarative way</h1>);
+}
+
+const items = (
+    <div>
+        <Maincontent />
+        <Decl />
+    </div>
+)
+ReactDOM.render(
+    items,
+    document.getElementById("root")
+)
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way";
+h1.className = "header";
+document.getElementById("root").append(h1);
